10ebad2e3ccb3aa3da53c7c9e846dae5c57817a1
BlueJ1 <lenny103@arcor.de>
2022-07-28 13:12:16 +0200

    Debugging

--- src/search.py
+++ src/search.py
@@ -6,8 +6,8 @@ from avoid import avoid_obstacles
 from utils import deep_copy, head_body_distance
 
 direction_dxdy = {"up": (0, 1), "down": (0, -1), "right": (1, 0), "left": (-1, 0)}
-CONSIDERED_DISTANCE = 12
-DEPTH_LIMIT = 15
+DEPTH_LIMIT = 4
+CONSIDERED_DISTANCE = int(1.5 * DEPTH_LIMIT)
 
 
 def remove_certain_deaths(state: dict, possible_moves: List[str], l: int = DEPTH_LIMIT) -> List[str]:
