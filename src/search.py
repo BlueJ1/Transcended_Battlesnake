b95e5852d5386bbea5f1513fb8a617df7898b4ff
BlueJ1 <lenny103@arcor.de>
2022-07-29 09:34:28 +0200

    Debugging

--- src/search.py
+++ src/search.py
@@ -1,17 +1,17 @@
 from typing import List
 import itertools
-from time import time
+# from time import time
 
 from avoid import avoid_obstacles
 from utils import deep_copy, head_body_distance, get_snake
 
-direction_dxdy = {"up": (0, 1), "down": (0, -1), "right": (1, 0), "left": (-1, 0)}
+move_direction = {"up": (0, 1), "down": (0, -1), "right": (1, 0), "left": (-1, 0)}
 DEPTH_LIMIT = 4
 CONSIDERED_DISTANCE = int(1.5 * DEPTH_LIMIT)
 
 
 def remove_certain_deaths(state: dict, possible_moves: List[str], l: int = DEPTH_LIMIT) -> List[str]:
-    t = time()
+    # t = time()
     my_id = state["you"]["id"]
     for move in possible_moves:
         move_possible = True
@@ -61,7 +61,7 @@ def dls_survival(state: dict, d: int, l: int):
 def simulate_turn(my_move: str, my_id: str, state: dict) -> List[dict]:
     my_snake = get_snake(my_id, state["board"]["snakes"])
     state = deep_copy(state)
-    state = simulate_move(my_move, my_snake, state)
+    simulate_move(my_move, my_snake, state)
 
     considered_snakes = []
     for snake in state["board"]["snakes"]:
@@ -75,10 +75,9 @@ def simulate_turn(my_move: str, my_id: str, state: dict) -> List[dict]:
     possible_outcomes = []
 
     for moves in itertools.product(*moves_considered_snakes):
-        print("Hello")
         new_state = deep_copy(state)
         for move, snake in moves:
-            new_state = simulate_move(move, snake, state)
+            simulate_move(move, snake, state)
 
         """
         # check head collisions
@@ -97,9 +96,7 @@ def simulate_turn(my_move: str, my_id: str, state: dict) -> List[dict]:
         if alive(new_state["board"]["snakes"], new_state["you"]["id"]):
             possible_outcomes.append(new_state)
 
-    print(possible_outcomes)
     if len(possible_outcomes) == 0:
-        print("Only one snake in the game.")
         possible_outcomes = [state]
 
     return possible_outcomes
@@ -113,9 +110,10 @@ def alive(snakes, snake_id):
     return any([snake_id == snake["id"] for snake in snakes])
 
 
-def simulate_move(move: str, snake: dict, state: dict) -> dict:
+def simulate_move(move: str, snake: dict, state: dict):
+    print(snake)
     head = snake["head"]
-    new_head = {"x": head["x"] + direction_dxdy[move][0], "y": head["y"] + direction_dxdy[move][1]}
+    new_head = {"x": head["x"] + move_direction[move][0], "y": head["y"] + move_direction[move][1]}
     snake["body"] = [new_head] + snake["body"][:-1]
 
     snake["head"] = new_head
@@ -132,4 +130,4 @@ def simulate_move(move: str, snake: dict, state: dict) -> dict:
         state["you"] = snake
         state["turn"] += 1
 
-    return state
+    print(snake)
